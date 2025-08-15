d685498768cd35c7c883f81b2f2d080312a6b648
CreamyWare <svedonikadrian@gmail.com>
2025-08-15 10:04:06 +0200

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -12443,51 +12443,6 @@ run(function()
 end)																																																																																																											
 
 run(function()
-    local ClientCrasher
-    local Method
-
-    ClientCrasher = vape.Categories.Minigames:CreateModule({
-        Name = 'Crasher',
-        Function = function(call)
-            if call then
-                for _, v in getconnections(game:GetService("ReplicatedStorage"):WaitForChild("events-@easy-games/game-core:shared/game-core-networking@getEvents.Events"):WaitForChild("abilityUsed").OnClientEvent) do
-                    v:Disconnect()    
-                end
-
-                ClientCrasher:Clean(collectionService:GetInstanceAddedSignal('inventory-entity'):Connect(function(player: Model)
-                    local item = player:WaitForChild('HandInvItem') :: IntValue?
-                    for i,v in getconnections(item.Changed) do
-                        v:Disable()
-                    end                
-                end))
-
-                repeat
-                    if entitylib.isAlive then
-                        if Method.Value == 'Ability' then
-                            for _ = 1, 1525 do
-                                replicatedStorage['events-@easy-games/game-core:shared/game-core-networking@getEvents.Events'].useAbility:FireServer('oasis_swap_staff')
-                            end
-                            task.wait(0.01)
-                        elseif Method.Value == 'Item' then
-                            for _, tool in store.inventory.inventory.items do
-                                task.spawn(switchItem, tool.tool, 0, true)
-                            end
-                        end
-                    end
-                    task.wait()
-
-                until not ClientCrasher.Enabled
-            end
-        end
-    })
-
-    Method = ClientCrasher:CreateDropdown({
-        Name = 'Method',
-        List = {'Item', 'Ability'}
-    })
-end)
-
-run(function()
 	local MouseTP
 	local Mode
 	local MovementMode
